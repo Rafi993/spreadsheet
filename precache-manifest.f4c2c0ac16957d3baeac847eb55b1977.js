2ee1101420f87b0389bf3bb97af9d8e7ff899168
rafi993 <rafi993@protonmail.com>
2020-02-28 12:03:01 +0530

    Initial build

---
+++ precache-manifest.f4c2c0ac16957d3baeac847eb55b1977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4174499b509abf838c88f29e01084b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995e5b1adb7e9e8b223c",
+    "url": "/static/js/2.90c9d28e.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.90c9d28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab66d34c543a26b2c0f",
+    "url": "/static/js/main.e0395c03.chunk.js"
+  },
+  {
+    "revision": "a99c899d333c019ce24b",
+    "url": "/static/js/runtime-main.5e15b942.js"
+  }
+]);
